02c52821a49dbd81423ee3b2c19507827ebd4bd9
Max <maximilienlarue@gmail.com>
2020-10-06 18:30:32 +0200

    fix typos

--- aclgroups/apps.py
+++ aclgroups/apps.py
@@ -4,7 +4,7 @@ from django.db.utils import OperationalError
 from aclgroups.acls import create_groups, set_models_acl
 
 
-class AclgroupsConfig(AppConfig):
+class AclGroupsConfig(AppConfig):
     name = 'aclgroups'
 
     def ready(self):
@@ -16,7 +16,7 @@ class AclgroupsConfig(AppConfig):
             acls = []
 
             if hasattr(settings, "ACL_GROUPS"):
-                acl_settings = getattr(settings, "ACL_GROUPS"):
+                acl_settings = getattr(settings, "ACL_GROUPS")
                 groups = getattr(acl_settings, "GROUPS", [])
                 acls = getattr(acl_settings, "ACL_DEFINITIONS", [])
 
